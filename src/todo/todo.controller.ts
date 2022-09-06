e5c99a1baa057f4af6dc386cfe1fd11e366d87cb
伊東和成 <itokazunari5375@icloud.com>
2022-09-06 09:37:13 +0900

    add: taskのコントローラーを追加

--- src/todo/todo.controller.ts
+++ src/todo/todo.controller.ts
@@ -1,4 +1,28 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  ParseIntPipe,
+  Patch,
+  Post,
+  UseGuards,
+  Req,
+} from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { Request } from 'express';
+import { Task } from '@prisma/client';
+import { TodoService } from './todo.service';
 
+@UseGuards(AuthGuard('jwt'))
 @Controller('todo')
-export class TodoController {}
+export class TodoController {
+  constructor(private readonly todoService: TodoService) {}
+  @Get()
+  getTasks(@Req() req: Request): Promise<Task[]> {
+    return this.todoService.getTasks(req.user.id);
+  }
+}
